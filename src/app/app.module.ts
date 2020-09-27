948d530c882aacc548becb0847199d89f49aa115
IvnH <idherrera@uniguajira.edu.co>
2020-09-26 21:32:11 -0500

    Servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    PruebaComponent
+    PruebaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/other/interfaces.ts
@@ -0,0 +1,18 @@
+
+export interface LoginResponse {
+    success: boolean
+    user: User
+    token: string
+}
+
+export interface User {
+    name: string
+    avatar: string
+    level: number
+    points: number
+    achievements: any[]
+    _id: string
+    type: string
+    user: string
+    email: string
+}
